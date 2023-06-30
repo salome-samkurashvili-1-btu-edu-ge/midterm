8dd277451f913564b26e45c1c77c7e264267e9f8
salome-samkurashvili-1-btu-edu-ge <131522209+salome-samkurashvili-1-btu-edu-ge@users.noreply.github.com>
2023-06-30 19:12:50 +0400

    Add files via upload

---
+++ test_quadratic_equation.py
@@ -0,0 +1,41 @@
+from quadratic_equation import QuadraticEquationSolver
+from pytest import approx, raises
+
+
+def test_0_0_0():
+    with raises(ZeroDivisionError) as e:
+        QuadraticEquationSolver(0, 0, 0).solve()
+
+
+def test_p1_0_0():
+    assert QuadraticEquationSolver(1, 0, 0).solve() == [0]
+
+
+def test_p1_p1_0():
+    assert QuadraticEquationSolver(1, 1, 0).solve() == [0, -1]
+
+
+def test_p1_n1_0():
+    assert QuadraticEquationSolver(1, -1, 0).solve() == [1, 0]
+
+
+def test_p1_p1_p1():
+    assert QuadraticEquationSolver(1, 1, 1).solve() == []
+
+
+def test_p1_p1_n1():
+    assert QuadraticEquationSolver(1, 1, -1).solve() == [
+        approx(-0.5 + 5**0.5 * 0.5),
+        approx(-0.5 - 5**0.5 * 0.5),
+    ]
+
+
+def test_p1_n1_p1():
+    assert QuadraticEquationSolver(1, -1, 1).solve() == []
+
+
+def test_p1_n1_n1():
+    assert QuadraticEquationSolver(1, -1, -1).solve() == [
+        approx(+0.5 - 5**0.5 * 0.5),
+        approx(+0.5 - 5**0.5 * 0.5),
+    ]
